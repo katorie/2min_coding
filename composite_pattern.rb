72b5aac0bd4cbd9e78b67d973f7f798c75ef54a0
Rie Kato <katoriexxxkatorie@gmail.com>
2021-05-14 16:03:31 +0900

    Make composite class

--- composite_pattern.rb
+++ composite_pattern.rb
@@ -20,6 +20,16 @@ class AddDryIngredientsTask < Task
   end
 end
 
+class AddLiquidsTask < Task
+  def initialize
+    super('Add liquids')
+  end
+
+  def get_time_required
+    1.0
+  end
+end
+
 class MixTask < Task
   def initialize
     super('Mix taht batter up!')
@@ -27,5 +37,46 @@ class MixTask < Task
 
   def get_time_required
     3.0
+  end
+end
+
+class CompositeTask < Task
+  def initialize
+    super(name)
+    @sub_tasks = []
+  end
+
+  def add_sub_task(task)
+    @sub_tasks << task
+  end
+  
+  def remove_sub_task(task)
+    @sub_tasks.delete(task)
+  end
+
+  def get_time_required
+    time = 0.0
+    @sub_tasks.each {|task| time += task.get_time_required }
+    time
+  end
+end
+
+class MakeBatterTask < CompositeTask
+  def initialize
+    super('Make batter')
+    add_sub_task(AddDryIngredientsTask.new)
+    add_sub_task(AddLiquidsTask.new)
+    add_sub_task(MixTask.new)
+  end
+end
+
+class MakeCakeTask < CompositeTask
+  def initialize
+    super('Make cake')
+    add_sub_task(MakeBatterTask.new)
+    add_sub_task(FillPanTask.new)
+    add_sub_task(BakeTask.new)
+    add_sub_task(FrostTask.new)
+    add_sub_task(LickSpoonTask.new)
   end
 end
