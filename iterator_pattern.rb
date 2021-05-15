f5f2c84a3e1f0b1226308c1d54f405277ab5f873
Rie Kato <katoriexxxkatorie@gmail.com>
2021-05-16 01:40:27 +0900

    内部イテレータ

--- iterator_pattern.rb
+++ iterator_pattern.rb
@@ -29,3 +29,15 @@ i = ArrayIterator.new('abc')
 while i.has_next?
   pp "item: #{i.next_item}"
 end
+
+def for_each_element(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+end
+
+a = [10, 20, 30]
+for_each_element(a) {|element| pp "The element is #{element}" }
+a.each {|element| pp "The element is #{element}" }
