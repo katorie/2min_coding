1081dfe61704592d62c61222526c8f336c143ecd
Rie Kato <katoriexxxkatorie@gmail.com>
2021-05-22 02:13:01 +0900

    madeleine

--- try_to_use_madeleine.rb
+++ try_to_use_madeleine.rb
@@ -25,7 +25,7 @@ class EmployeeManager
 
   def change_address(number, address)
     employee = @employees[number]
-    raise "No such employee" if not employee
+    raise "No such employee" unless employee
     employee.address = address
   end
 
@@ -34,7 +34,7 @@ class EmployeeManager
   end
 
   def find_employee(number)
-    @employees(number)
+    @employees[number]
   end
 end
 
@@ -68,3 +68,29 @@ class ChangeAddress
     system.change_address(number, address)
   end
 end
+
+class FindEmployee 
+  def initialize(number)
+    @number = number
+  end
+
+  def execute(system)
+    system.find_employee(number)
+  end
+end
+
+store = SnapshotMadeleine.new('employees') { EmployeeManager.new }
+Thread.new do
+  while true
+    sleep 20
+    madeleine.take_snapshot
+  end
+end
+
+tom = Employee.new('tom', '1001', '1, Division St.')
+harry = Employee.new('harry', '1002', '3435 Sunnyside Ave')
+
+store.execute_command(AddEmployee.new(tom))
+store.execute_command(AddEmployee.new(harry))
+puts store.execute_command(FindEmployee.new('1001'))
+puts store.execute_command(FindEmployee.new('1002'))
