04edf017ae13b54cf082287072de71fe4aff1dd4
Rie Kato <katoriexxxkatorie@gmail.com>
2021-06-04 12:52:28 +0900

    decorator

--- decorator.rb
+++ decorator.rb
@@ -31,3 +31,80 @@ class EnhancedWriter
     @file.close
   end
 end
+
+class SimpleWriter
+  def initialize(path)
+    @file = File.open(path, "w")
+  end
+
+  def write_line(line)
+    @file.print(line)
+    @file.print("\n")
+  end
+
+  def pos
+    @file.pos
+  end
+
+  def rewind
+    @file.rewind
+  end
+
+  def close
+    @file.close
+  end
+end
+
+class WriteDecorator
+  def initialize(real_writer)
+    @real_writer = real_writer
+  end
+
+  def write_line(line)
+    @real_writer.write_line(line)
+  end
+
+  def pos
+    @real_writer.pos
+  end
+
+  def rewind
+    @real_writer.rewind
+  end
+  
+  def close
+    @real_writer.close
+  end
+end
+
+class NumberingWriter < WriterDecorator
+  def initialize(real_writer)
+    super(real_writer)
+    @line_number = 1
+  end
+
+  def writer_line(line)
+    @real_writer.write_line("#{@line_number}: #{line}")
+  end
+end
+
+class CheckSummingWriter < WriterDecorator
+  attr_reader :check_sum
+
+  def initialize(real_writer)
+    @real_writer = real_writer
+    @check_sum = 0
+  end
+
+  def write_line(line)
+    line.each_byte {|byte| @check_sum = (@check_sum + byte) % 256 }
+    @check_sum += "\n"[0] % 256
+    @real_writer.write_line(line)
+  end
+end
+
+class TimeStampingWriter < WriterDecorator
+  def write_ilne(line)
+    @real_writer.write_line("#{Time.new}: #{line}")
+  end
+end
