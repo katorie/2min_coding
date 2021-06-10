c96b79360d03a38997345ba12d1f8d3c758a1ed8
Rie Kato <katoriexxxkatorie@gmail.com>
2021-06-10 13:35:45 +0900

    singleton

--- singleton.rb
+++ singleton.rb
@@ -22,3 +22,40 @@ puts "c1>> #{c1}"
 
 c2 = ClassVariableTester.new
 puts "c2>> #{c2}"
+
+
+class SimpleLogger
+  attr_accessor :level
+
+  ERROR = 1
+  WARNING = 2
+  INFO = 3
+
+  def initialize
+    @log = File.open("log.txt", "w")
+    @level = WARNING
+  end
+
+  def error(msg)
+    @log.puts(msg)
+    @log.flush
+  end
+
+  def warning(msg)
+    @log.puts(msg) if @level >= WARNING
+    @log.flush
+  end
+
+  def info(msg)
+    @log.puts(msg) if @level >= INFO
+    @log.flush
+  end
+
+  @@instance = SimpleLogger.new
+
+  def self.instance
+    return @@instance
+  end
+
+  private_class_method :new
+end
