4bc127a17c9193d5758164142fe3e88e1056751c
Rie Kato <katoriexxxkatorie@gmail.com>
2021-06-14 16:23:35 +0900

    singleton

--- singleton.rb
+++ singleton.rb
@@ -59,3 +59,39 @@ class SimpleLogger
 
   private_class_method :new
 end
+
+class ClassBasedLogger
+  ERROR = 1
+  WARNING = 2
+  INFO = 3
+
+  @@log = File.open('log.txt', 'w')
+  @@level = WARNING
+
+  def self.error(msg)
+    @@log.puts(msg)
+    @@log.flush
+  end
+
+  def self.warning(msg)
+    @@log.puts(msg) if @@level >= WARNING
+    @@log.flush
+  end
+
+  def self.info(msg)
+    @@log.puts(msg) if @@level >= INFO
+  end
+
+  def self.level=(new_level)
+    @@level = new_level
+  end
+
+  def self.level
+    @@level
+  end
+end
+
+ClassBasedLogger.level = ClassBasedLogger::INFO
+
+ClassBasedLogger.info('コンピュータがチェスゲームに勝ちました')
+ClassBasedLogger.warning('ユニットAE-35の故障が予測されました')
