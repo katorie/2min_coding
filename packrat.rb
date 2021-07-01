d491dd2128d7cc1301c4c447a66ffcc1a5926d7f
Rie Kato <katoriexxxkatorie@gmail.com>
2021-07-01 11:46:25 +0900

    PackRat

--- packrat.rb
+++ packrat.rb
@@ -1,23 +1,6 @@
 require 'finder'
 
-def backup(dir, find_expression = All.new)
-  Backup.instance.data_sources << DataSource.new(dir, find_expression)
-end
-
-def to(backup_directory)
-  Backup.instance.backup_directory = backup_directory
-end
-
-def interval(minutes)
-  Backup.instance.interval = minutes
-end
-
-eval(File.read('backup.pr'))
-Backup.instance.run
-
 class Backup
-  include Singleton
-
   attr_accessor :backup_directory, :interval
   attr_reader :data_sources
 
@@ -25,17 +8,28 @@ class Backup
     @data_sources = []
     @backup_directory = '/backup'
     @interval = 60
+    yield(self) if block_given?
+    PackRat.instance.register_backup(self)
   end
 
-  def backup_files
-    this_backup_dir = Time.new.ctime.tr(' :', '_')
-    this_backup_path = File.join(backup_directory, this_backup_dir)
-    @data_sources.each {|source| source.backup(this_backup_path) }
+  def backup(dir, find_expression = All.new)
+    @data_sources << DataSource.new(dir, find_expression)
+  end
+
+  def to(backup_directory)
+    @backup_directory = backup_directory
+  end
+
+  def interval(minutes)
+    @interval = minutes
   end
 
   def run
     while true
-      backup_files
+      this_backup_dir = Time.new.ctime.tr(' :', '_')
+      this_backup_path = File.join(backup_directory, this_backup_dir)
+      @data_sources.each {|source| source.backup(this_backup_path) }
+
       sleep(@interval * 60)
     end
   end
@@ -62,3 +56,28 @@ class DataSource
     FileUtils.cp(path, copy_path)
   end
 end
+
+class PackRat
+  include Singleton
+
+  def initialize
+    @backups = []
+  end
+
+  def register_backup(backup)
+    @backupts << backup
+  end
+
+  def run
+    threads = []
+
+    @backups.each do |backup|
+      threads << Thread.new {backup.run }
+    end
+
+    threads.each {|t| t.join }
+  end
+end
+
+eval(File.read('backup.pr'))
+PackRat.instance.run
