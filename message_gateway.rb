482fbc29d07a89efc20c3ed5ee5095ebb25d3c22
Rie Kato <katoriexxxkatorie@gmail.com>
2021-07-04 22:23:52 +0900

    message gateway

--- message_gateway.rb
+++ message_gateway.rb
@@ -42,3 +42,27 @@ class FileAdapter
     end
   end
 end
+
+class MessageGateway
+  def initialize
+    load_adapters
+  end
+
+  def process_message(message)
+    adapter = adapter_for(message)
+    adapter.send_message(message)
+  end
+
+  def adapter_for(message)
+    protocol = message.to.scheme
+    adapter_name = protocol.capitalize + 'Adapter'
+    adapter_class = self.class.const_get(adapter_name)
+    adapter_class.new
+  end
+
+  def load_adapters
+    lib_dir = File.dirname(__FILE__)
+    full_pattern = File.join(lib_dir, 'adapter', '*.rb')
+    Dir.glob(full_pattern).each {|file| require file }
+  end
+end
