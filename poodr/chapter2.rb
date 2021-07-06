9c83c897525cf0d27ea9431530201481206440ef
Rie Kato <katoriexxxkatorie@gmail.com>
2021-07-06 14:31:26 +0900

    RevealingReferences

--- poodr/chapter2.rb
+++ poodr/chapter2.rb
@@ -16,3 +16,20 @@ class Gear
     ratio * (rim + (tire * 2))
   end
 end
+
+class RevealingReferences
+  attr_reader :wheels
+
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect {|wheel| wheel.rim + (wheel.tire * 2)}
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    data.collect {|cell| wheel.new(cell[0], cell[1])}
+  end
+end
